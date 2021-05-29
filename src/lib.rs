4c470eac281c96c40eee3d9b6f09a129aac02df0
catsworld <519418441@qq.com>
2021-05-29 20:15:35 +0800

    feat: initialize the repository and define basic data types

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub struct Bot {}
+
+pub struct Message {}
+
+pub struct MessageSegment {}
+
+pub enum Event {}
+
+pub struct MessageEvent {}
+
+pub struct NoticeEvent {}
+
+pub struct RequestEvent {}
+
+pub struct MetaEvent {}
+
+pub struct User {}
+
+pub struct Group {}
+
+pub struct File {}
