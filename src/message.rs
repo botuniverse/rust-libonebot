5cf520021b3319c38ce0529eaf1dd4bd37b613df
catsworld <519418441@qq.com>
2021-09-28 15:40:59 +0800

    feat: add more methods for building message segmants

--- src/message.rs
+++ src/message.rs
@@ -1,6 +1,6 @@
 use crate::{Group, User};
 use serde::Serialize;
-use std::{collections::HashMap, fmt::Display};
+use std::fmt::Display;
 
 #[derive(Debug, Clone)]
 pub struct Message {
@@ -41,6 +41,51 @@ impl Message {
         self.content.push(MessageSegment::Text(text.to_string()));
         self
     }
+
+    pub fn emoji<S: Display>(mut self, emoji: S) -> Self {
+        self.content.push(MessageSegment::Emoji(emoji.to_string()));
+        self
+    }
+
+    pub fn image(mut self, image: Media) -> Self {
+        self.content.push(MessageSegment::Image(image));
+        self
+    }
+
+    pub fn record(mut self, record: Media) -> Self {
+        self.content.push(MessageSegment::Record(record));
+        self
+    }
+
+    pub fn video(mut self, video: Media) -> Self {
+        self.content.push(MessageSegment::Video(video));
+        self
+    }
+
+    pub fn at(mut self, user: User) -> Self {
+        self.content.push(MessageSegment::At(user));
+        self
+    }
+
+    pub fn location(mut self, lat: f64, lon: f64) -> Self {
+        self.content.push(MessageSegment::Location(lat, lon));
+        self
+    }
+
+    pub fn reply<S: Display>(mut self, id: S) -> Self {
+        self.content.push(MessageSegment::Reply(id.to_string()));
+        self
+    }
+
+    pub fn forward<S: Display>(mut self, id: S) -> Self {
+        self.content.push(MessageSegment::Forward(id.to_string()));
+        self
+    }
+
+    pub fn custom<S: Display>(mut self, s: S) -> Self {
+        self.content.push(MessageSegment::Custom(s.to_string()));
+        self
+    }
 }
 
 pub struct MessageBuilder {
@@ -72,6 +117,51 @@ impl MessageBuilder {
         self.content.push(MessageSegment::Text(text.to_string()));
         self
     }
+
+    pub fn emoji<S: Display>(mut self, emoji: S) -> Self {
+        self.content.push(MessageSegment::Emoji(emoji.to_string()));
+        self
+    }
+
+    pub fn image(mut self, image: Media) -> Self {
+        self.content.push(MessageSegment::Image(image));
+        self
+    }
+
+    pub fn record(mut self, record: Media) -> Self {
+        self.content.push(MessageSegment::Record(record));
+        self
+    }
+
+    pub fn video(mut self, video: Media) -> Self {
+        self.content.push(MessageSegment::Video(video));
+        self
+    }
+
+    pub fn at(mut self, user: User) -> Self {
+        self.content.push(MessageSegment::At(user));
+        self
+    }
+
+    pub fn location(mut self, lat: f64, lon: f64) -> Self {
+        self.content.push(MessageSegment::Location(lat, lon));
+        self
+    }
+
+    pub fn reply<S: Display>(mut self, id: S) -> Self {
+        self.content.push(MessageSegment::Reply(id.to_string()));
+        self
+    }
+
+    pub fn forward<S: Display>(mut self, id: S) -> Self {
+        self.content.push(MessageSegment::Forward(id.to_string()));
+        self
+    }
+
+    pub fn custom<S: Display>(mut self, s: S) -> Self {
+        self.content.push(MessageSegment::Custom(s.to_string()));
+        self
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -89,14 +179,28 @@ pub enum MessageSegment {
     Video(Media),
     At(User),
     Location(f64, f64), // lat, lon
-    Reply(i64),
-    Foward(i64),
-    Custom(HashMap<String, String>),
+    Reply(String),
+    Forward(String),
+    Custom(String),
 }
 
 #[derive(Debug, Clone, Serialize)]
 pub enum Media {
     File(String),
-    URL(String, bool, bool, bool), // url, cache, proxy, timeout
+    URL(String), // url, cache, proxy, timeout
     Base64(String),
+}
+
+impl Media {
+    pub fn new_file<S: Display>(file: S) -> Self {
+        Self::File(file.to_string())
+    }
+
+    pub fn new_url<S: Display>(url: S) -> Self {
+        Self::URL(url.to_string())
+    }
+
+    pub fn new_base64<S: Display>(base64: S) -> Self {
+        Self::Base64(base64.to_string())
+    }
 }
